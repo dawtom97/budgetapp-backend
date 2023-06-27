c029967cf8429f8dd109c1f8ef9c52a433e87363
Dawid <dawtom97@gmail.com>
2023-06-27 12:58:25 +0200

    add new expense query and expenses sum

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -17,11 +17,36 @@ router.get("/", authorization, async (req,res) => {
 router.get("/expenses", authorization, async (req,res) => {
     try {
         const {rows} = await pool.query("SELECT * FROM expenses WHERE user_id = $1",[req.user]);
+
         res.json({expenses:rows})
     } catch (error) {
         console.log(error.message);
         res.status(500).json("Server error") 
     }
 })
+
+router.get("/expenses-sum", authorization, async (req,res) => {
+    try {
+        const {rows} = await pool.query("SELECT SUM(expense_amount) FROM expenses WHERE user_id = $1",[req.user]);
+        res.json({balance:rows[0]})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json("Server error") 
+    }
+})
+
+router.post("/expenses-add", authorization, async (req,res) => {
+    try {
+        const {category,title,amount} = req.body;
+        const {rows} = await pool.query("INSERT INTO expenses(user_id, expense_category, expense_title, expense_amount, expense_date) VALUES ($1, $2, $3, $4, CURRENT_DATE) RETURNING *",[req.user,category, title,amount ]);
+
+        res.json({expense:rows[0], success:true});
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({success:false}) 
+    }
+})
+
 
 module.exports = router;
